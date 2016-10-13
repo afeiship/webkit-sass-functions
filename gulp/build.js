b32b6437864676e38e611356ed7c4e3140281111
feizheng <1290657123@qq.com>
2016-10-13 16:23:09 +0800

    update dist

--- gulp/build.js
+++ gulp/build.js
@@ -10,8 +10,8 @@
   });
 
   gulp.task('build', ['clean'], function() {
-    console.log('Your task goes here!');
-    //gulp.start(['styles']);
+    // console.log('Your task goes here!');
+    gulp.start(['styles']);
   });
 
 
